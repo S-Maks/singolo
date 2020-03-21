d793b96c5651acaa8b453535c95332a1cfb859cb
S-Maks <cool.dan47@mail.ru>
2020-03-21 22:50:38 +0300

    fix: click button

--- script.js
+++ script.js
@@ -10,10 +10,16 @@ for (let anchor of anchors){
 
 
 //Phone animation
-let themeButton = document.querySelector('.vertical-circle');
-themeButton.onclick = function(){
+let Button = document.querySelector('.vertical-circle');
+Button.onclick = function(){
     if(document.querySelector('.vertical-blackRect').classList.length==2)
     document.querySelector('.vertical-blackRect').classList.remove('display-of-iphones');
 else document.querySelector('.vertical-blackRect').classList.add('display-of-iphones');
+}
 
+let Button1 = document.querySelector('.vertical-square');
+Button1.onclick = function(){
+    if(document.querySelector('.vertical-blackRect').classList.length==2)
+    document.querySelector('.vertical-blackRect').classList.remove('display-of-iphones');
+else document.querySelector('.vertical-blackRect').classList.add('display-of-iphones');
 }
