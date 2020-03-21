0a4245289e50da9ed59bb15600655db8fe08f43d
S-Maks <cool.dan47@mail.ru>
2020-03-21 22:45:15 +0300

    feat: add animation off vertical phone

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ for (let anchor of anchors){
             {behavior:'smooth', block: 'start'});
     })
 }
+
+
+//Phone animation
+let themeButton = document.querySelector('.vertical-circle');
+themeButton.onclick = function(){
+    if(document.querySelector('.vertical-blackRect').classList.length==2)
+    document.querySelector('.vertical-blackRect').classList.remove('display-of-iphones');
+else document.querySelector('.vertical-blackRect').classList.add('display-of-iphones');
+
+}
