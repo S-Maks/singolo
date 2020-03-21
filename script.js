6ad890e88d78597302ddaaa489b20e2e02b10774
S-Maks <cool.dan47@mail.ru>
2020-03-21 23:02:16 +0300

    feat: add animation horizontal phone

--- script.js
+++ script.js
@@ -10,16 +10,32 @@ for (let anchor of anchors){
 
 
 //Phone animation
-let Button = document.querySelector('.vertical-circle');
-Button.onclick = function(){
+//Vertical
+let Button__Vertical = document.querySelector('.vertical-circle');
+Button__Vertical.onclick = function(){
     if(document.querySelector('.vertical-blackRect').classList.length==2)
     document.querySelector('.vertical-blackRect').classList.remove('display-of-iphones');
 else document.querySelector('.vertical-blackRect').classList.add('display-of-iphones');
 }
 
-let Button1 = document.querySelector('.vertical-square');
-Button1.onclick = function(){
+let Button1__Vertical = document.querySelector('.vertical-square');
+Button1__Vertical.onclick = function(){
     if(document.querySelector('.vertical-blackRect').classList.length==2)
     document.querySelector('.vertical-blackRect').classList.remove('display-of-iphones');
 else document.querySelector('.vertical-blackRect').classList.add('display-of-iphones');
+}
+
+//Horizontal
+let Button__Horizontal = document.querySelector('.horizontal-circle');
+Button__Horizontal.onclick = function(){
+    if(document.querySelector('.horizontal-blackRect').classList.length==2)
+    document.querySelector('.horizontal-blackRect').classList.remove('display-of-iphones');
+else document.querySelector('.horizontal-blackRect').classList.add('display-of-iphones');
+}
+
+let Button1__Horizontal = document.querySelector('.horizontal-square');
+Button1__Horizontal.onclick = function(){
+    if(document.querySelector('.horizontal-blackRect').classList.length==2)
+    document.querySelector('.horizontal-blackRect').classList.remove('display-of-iphones');
+else document.querySelector('.horizontal-blackRect').classList.add('display-of-iphones');
 }
