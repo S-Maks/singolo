ac3df03c795f5ac9f56eb555bc26192be62d69bb
S-Maks <cool.dan47@mail.ru>
2020-03-21 23:45:52 +0300

    feat: add border photo

--- script.js
+++ script.js
@@ -39,3 +39,82 @@ Button1__Horizontal.onclick = function(){
     document.querySelector('.horizontal-blackRect').classList.remove('display-of-iphones');
 else document.querySelector('.horizontal-blackRect').classList.add('display-of-iphones');
 }
+
+let page = document.querySelector('.list-img');
+let themeButton = document.querySelector('.list-img');
+themeButton.onclick = function() {
+    page.classList.toggle('border-off');
+  page.classList.toggle('border-img');
+};
+
+let page1 = document.querySelector('.list1-img');
+let themeButton1 = document.querySelector('.list1-img');
+themeButton1.onclick = function() {
+    page1.classList.toggle('border-off');
+  page1.classList.toggle('border-img');
+};
+
+let page2 = document.querySelector('.list2-img');
+let themeButton2 = document.querySelector('.list2-img');
+themeButton2.onclick = function() {
+    page2.classList.toggle('border-off');
+  page2.classList.toggle('border-img');
+};
+let page3 = document.querySelector('.list3-img');
+let themeButton3 = document.querySelector('.list3-img');
+themeButton3.onclick = function() {
+    page3.classList.toggle('border-off');
+  page3.classList.toggle('border-img');
+};
+let page4 = document.querySelector('.list4-img');
+let themeButton4 = document.querySelector('.list4-img');
+themeButton4.onclick = function() {
+    page4.classList.toggle('border-off');
+    page4.classList.toggle('border-img');
+};
+let page5 = document.querySelector('.list5-img');
+let themeButton5 = document.querySelector('.list5-img');
+themeButton5.onclick = function() {
+    page5.classList.toggle('border-off');
+    page5.classList.toggle('border-img');
+};
+let page6 = document.querySelector('.list6-img');
+let themeButton6 = document.querySelector('.list6-img');
+themeButton6.onclick = function() {
+    page6.classList.toggle('border-off');
+    page6.classList.toggle('border-img');
+};
+
+let page7 = document.querySelector('.list7-img');
+let themeButton7 = document.querySelector('.list7-img');
+themeButton7.onclick = function() {
+    page7.classList.toggle('border-off');
+    page7.classList.toggle('border-img');
+};
+
+let page8 = document.querySelector('.list8-img');
+let themeButton8 = document.querySelector('.list8-img');
+themeButton8.onclick = function() {
+    page8.classList.toggle('border-off');
+    page8.classList.toggle('border-img');
+};
+
+let page9 = document.querySelector('.list9-img');
+let themeButton9 = document.querySelector('.list9-img');
+themeButton9.onclick = function() {
+    page9.classList.toggle('border-off');
+    page9.classList.toggle('border-img');
+};
+
+let page10 = document.querySelector('.list10-img');
+let themeButton10 = document.querySelector('.list10-img');
+themeButton10.onclick = function() {
+    page10.classList.toggle('border-off');
+    page10.classList.toggle('border-img');
+};
+let page11 = document.querySelector('.list11-img');
+let themeButton11 = document.querySelector('.list11-img');
+themeButton11.onclick = function() {
+    page11.classList.toggle('border-off');
+    page11.classList.toggle('border-img');
+};
